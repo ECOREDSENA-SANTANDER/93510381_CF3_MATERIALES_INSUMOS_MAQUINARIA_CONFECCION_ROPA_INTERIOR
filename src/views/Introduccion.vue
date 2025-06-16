5c9f6a7d447aaa46691c57804809180de4e20bd2
lizmanchego <karinamanchego0416@gmail.com>
2025-06-16 10:29:11 -0500

    [ADD] Video Intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -17,8 +17,9 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/4hvtzTzBvjQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Maquinaria, herramientas y ajustes para la confección de ropa interior
+
 
 </template>
 
