a7c680ac77b6774176408c0d8374270aac0e7144
lizmanchego <karinamanchego0416@gmail.com>
2025-07-21 15:03:40 -0500

    [ADD] CFA PDF + WORD

--- src/config/global.js
+++ src/config/global.js
@@ -146,7 +146,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF3_93510381_DU.pdf',
       },
       {
         icono: 'fas fa-download',
